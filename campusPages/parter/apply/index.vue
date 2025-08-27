52b319ef26c60f6eb4b897cb2f6f17a56736f431
shenpeng <1367291468@qq.com>
2025-08-27 14:11:50 +0800

    页面转化

--- campusPages/parter/apply/index.vue
+++ campusPages/parter/apply/index.vue
@@ -1,18 +1,543 @@
 <template>
-  
+  <view class="apply-box">
+    <view class="wirpper">
+      <view class="content">
+        <view class="input-module">
+          <view class="same-title"
+            >您的真实姓名<text class="must">*</text></view
+          >
+          <view class="input-box">
+            <input
+              type="text"
+              maxlength="40"
+              class="same-input"
+              placeholder="请输入您的真实姓名"
+              :value="applyParams.reallyName"
+              bind:input="handleInputReallyName"
+            />
+          </view>
+        </view>
+        <view class="input-module">
+          <view class="same-title">您的性别<text class="must">*</text></view>
+          <view class="sex-row">
+            <view
+              class="radio-item mr120 sex-radio-item"
+              data-value="male"
+              @click="onChangeIsGender"
+            >
+              <image
+                :src="
+                  applyParams.gender == 'male'
+                    ? imgUrl + '/icons/icon-checkout-blue.png'
+                    : imgUrl + '/icons/icon-checkout-gray.png'
+                "
+                class="icon-checkbox"
+              />
+              <text class="radio-txt active-txt">男</text>
+            </view>
+            <view
+              class="radio-item"
+              data-value="female"
+              @click="onChangeIsGender"
+            >
+              <image
+                :src="
+                  applyParams.gender == 'female'
+                    ? imgUrl + '/icons/icon-checkout-blue.png'
+                    : imgUrl + '/icons/icon-checkout-gray.png'
+                "
+                class="icon-checkbox"
+              />
+              <text class="radio-txt">女</text>
+            </view>
+          </view>
+        </view>
+
+        <view class="input-module">
+          <view class="same-title"
+            >您的出生日期<text class="must">*</text></view
+          >
+          <picker
+            mode="date"
+            value="{{applyParams.birthday}}"
+            start="1990-01-01"
+            end="2045-09-01"
+            fields="day"
+            bindchange="bindBirthdayChange"
+            class="input-box"
+          >
+            <view class="default-data" v-if="applyParams.birthday === ''"
+              >请选择您的出生日期</view
+            >
+            <view class="birthday" v-if="applyParams.birthday">
+              {{ applyParams.birthday }}
+            </view>
+          </picker>
+        </view>
+
+        <view class="input-module">
+          <view class="same-title">您的手机号<text class="must">*</text></view>
+          <view class="input-box">
+            <input
+              type="number"
+              maxlength="11"
+              class="same-input"
+              placeholder="请输入您的手机号"
+              :value="applyParams.mobile"
+              bind:input="handleInputMobile"
+            />
+          </view>
+        </view>
+
+        <view class="input-module">
+          <view class="same-title">您的微信号<text class="must">*</text></view>
+          <view class="input-box">
+            <input
+              type="text"
+              maxlength="40"
+              class="same-input"
+              placeholder="请输入您的微信号"
+              :value="applyParams.wechatNum"
+              bind:input="handleInputWechatId"
+            />
+          </view>
+        </view>
+        <view class="input-module">
+          <view class="same-title"
+            >您的微信昵称<text class="must">*</text></view
+          >
+          <view class="input-box">
+            <input
+              type="text"
+              maxlength="40"
+              class="same-input"
+              placeholder="请输入您的微信昵称"
+              :value="applyParams.wechatName"
+              bind:input="handleInputWechatNick"
+            />
+          </view>
+        </view>
+        <view class="input-module">
+          <view class="same-title"
+            >您的电子邮箱<text class="must">*</text></view
+          >
+          <view class="input-box">
+            <input
+              type="text"
+              maxlength="40"
+              class="same-input"
+              placeholder="请输入您的电子邮箱"
+              :value="applyParams.email"
+              bind:input="handleInputEmail"
+            />
+          </view>
+        </view>
+        <view class="input-module">
+          <view class="same-title"
+            >您的最高学历<text class="must">*</text></view
+          >
+          <view class="subtitle">学历：大专，本科，硕士，博士及以上</view>
+          <view class="input-box">
+            <input
+              type="text"
+              maxlength="10"
+              class="same-input"
+              placeholder="请输入您的最高学历"
+              :value="applyParams.highestEdu"
+              bind:input="handleInputHightEdu"
+            />
+          </view>
+        </view>
+        <view class="input-module">
+          <view class="same-title"
+            >您当前所在的学校名称或者毕业院校名称（毕业两年以内）<text
+              class="must"
+              >*</text
+            ></view
+          >
+          <view class="input-box">
+            <input
+              type="text"
+              maxlength="60"
+              class="same-input"
+              placeholder="请输入您的学校名称"
+              :value="applyParams.schoolName"
+              bind:input="handleInputSchoolName"
+            />
+          </view>
+        </view>
+
+        <view class="input-module">
+          <view class="same-title"
+            >您的专业名称<text class="must">*</text></view
+          >
+          <view class="input-box">
+            <input
+              type="text"
+              maxlength="40"
+              class="same-input"
+              placeholder="请输入您的专业"
+              :value="applyParams.major"
+              bind:input="handleInputMajor"
+            />
+          </view>
+        </view>
+        <view class="input-module">
+          <view class="same-title"
+            >您的毕业时间<text class="must">*</text></view
+          >
+          <picker
+            mode="date"
+            :value="date"
+            start="2021-09-01"
+            end="2029-09-01"
+            bindchange="bindGradYearChange"
+            fields="year"
+            class="input-box"
+          >
+            <view class="default-data" v-if="applyParams.gradYear === ''"
+              >请选择您的毕业时间</view
+            >
+            <view class="birthday" v-if="applyParams.gradYear">
+              {{ applyParams.gradYear }}
+            </view>
+          </picker>
+        </view>
+
+        <view class="input-module">
+          <view class="same-title mb4"
+            >简单介绍一下您自己<text class="must">*</text></view
+          >
+          <view class="subtitle"
+            >简短介绍个人背景和兴趣，或创业过程，参与的项目经历、技术技能、期望收获等等，方便我们了解独特而有趣的您</view
+          >
+          <textarea
+            maxlength="800"
+            class="same-textarea"
+            placeholder="请输入"
+            :value="applyParams.introMyself"
+            bind:input="handleInputSelf"
+            placeholder-style="font-size:28rpx"
+          ></textarea>
+        </view>
+
+        <view class="input-module">
+          <view class="same-title"
+            >为什么想要成为我们的一员呢？<text class="must">*</text></view
+          >
+          <textarea
+            maxlength="800"
+            class="same-textarea"
+            placeholder="请输入"
+            :value="applyParams.joinReason"
+            bind:input="handleInputJoin"
+            placeholder-style="font-size:28rpx"
+          ></textarea>
+        </view>
+        <view class="input-module">
+          <view class="same-title"
+            >是否有推荐人<text class="must">*</text></view
+          >
+          <view class="sex-row">
+            <view
+              class="radio-item mr120 sex-radio-item"
+              data-value="Y"
+              @click="onChangeReferrer"
+            >
+              <image
+                :src="
+                  applyParams.hasReferrer == 'Y'
+                    ? imgUrl + '/icons/icon-checkout-blue.png'
+                    : imgUrl + '/icons/icon-checkout-gray.png'
+                "
+                class="icon-checkbox"
+              />
+              <text class="radio-txt active-txt">是</text>
+            </view>
+            <view class="radio-item" data-value="N" @click="onChangeReferrer">
+              <image
+                :src="
+                  applyParams.hasReferrer == 'N'
+                    ? imgUrl + '/icons/icon-checkout-blue.png'
+                    : imgUrl + '/icons/icon-checkout-gray.png'
+                "
+                class="icon-checkbox"
+              />
+              <text class="radio-txt">否</text>
+            </view>
+          </view>
+        </view>
+        <block v-if="applyParams.hasReferrer == 'Y'">
+          <view class="input-module">
+            <view class="same-title"
+              >推荐人姓名<text class="must">*</text></view
+            >
+            <view class="input-box">
+              <input
+                type="text"
+                maxlength="40"
+                class="same-input"
+                placeholder="请输入推荐人姓名"
+                :value="applyParams.referrerName"
+                bind:input="handleInputReferrerName"
+              />
+            </view>
+          </view>
+          <view class="input-module">
+            <view class="same-title mb4"
+              >推荐人联系方式<text class="must">*</text></view
+            >
+            <view class="subtitle">推荐人的手机号或者微信号</view>
+            <view class="input-box">
+              <input
+                type="text"
+                maxlength="40"
+                class="same-input"
+                placeholder="请输入推荐人联系方式"
+                :value="applyParams.referrerContact"
+                bind:input="handleInputRecontact"
+              />
+            </view>
+          </view>
+        </block>
+        <view class="input-module">
+          <view class="same-title"
+            >您通过什么方式了解到我们的<text class="must">*</text></view
+          >
+          <textarea
+            maxlength="500"
+            class="same-textarea"
+            placeholder="请输入"
+            :value="applyParams.howToKnow"
+            bind:input="handleInputHowknow"
+            placeholder-style="font-size:28rpx"
+          ></textarea>
+        </view>
+        <view class="input-module">
+          <view class="same-title">您想和我们说的悄悄话</view>
+          <textarea
+            maxlength="500"
+            class="same-textarea"
+            placeholder="请输入"
+            :value="applyParams.whisper"
+            bind:input="handleInputWhisper"
+            placeholder-style="font-size:28rpx"
+          ></textarea>
+        </view>
+        <view class="input-module">
+          <view class="same-title mb4">上传简历</view>
+          <view class="subtitle"
+            >上传的格式支持：pdf，word，doc，docx。内容大小小于10M</view
+          >
+          <view class="upload-btn" @click="uploadChooseFile">
+            <view class="choost-file" v-if="resumeFilePath == ''"
+              >+选择文件</view
+            >
+            <view v-if="resumeFilePath">
+              {{ resumeFileName }}
+            </view>
+          </view>
+        </view>
+      </view>
+    </view>
+    <view class="empty-box"></view>
+    <view class="foot-box">
+      <view class="submit-btn" @click="submitApplyData">提交</view>
+    </view>
+  </view>
 </template>
 
 <script>
+import { imgUrls } from "@/config/app";
+import { PARTNER_BASE_DATA } from "@/const/campus";
 export default {
   data: function () {
-    return {};
+    return {
+      imgUrl: imgUrls,
+      applyParams: PARTNER_BASE_DATA,
+      resumeFilePath: "",
+      resumeFileName: "",
+    };
   },
-  methods: {
-    
-  },
+  methods: {},
 };
 </script>
 
 <style lang="scss" scoped>
+page {
+  background: #f4f4f4;
+}
+.apply-box {
+  width: 100%;
+  position: relative;
+  .wirpper {
+    width: 100%;
+    box-sizing: border-box;
+    padding: 24rpx;
+    .content {
+      width: 100%;
+      border-radius: 24rpx;
+      background: #ffffff;
+      box-sizing: border-box;
+      padding: 32rpx 24rpx;
+      .input-module {
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 24rpx;
+        .same-title {
+          font-size: 32rpx;
+          color: #2f3031;
+          line-height: 48rpx;
+          font-weight: bold;
+          margin-bottom: 16rpx;
+        }
+        .must {
+          font-size: 32rpx;
+          color: #ff0000;
+          line-height: 48rpx;
+          font-weight: bold;
+          margin-left: 8rpx;
+        }
+        .input-box {
+          width: 100%;
+          height: 72rpx;
+          background: #ffffff;
+          border-radius: 12rpx;
+          border: 1rpx solid #e5e6e6;
+          position: relative;
+          .same-input {
+            width: 100%;
+            height: 100%;
+            box-sizing: border-box;
+            padding-left: 16rpx;
+            color: #2f3031;
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            font-size: 28rpx;
+          }
+        }
 
+        .sex-row {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          .radio-item {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            box-sizing: border-box;
+            padding: 1rpx 30rpx 1rpx 2rpx;
+          }
+          .icon-checkbox {
+            width: 24rpx;
+            height: 24rpx;
+            margin-right: 12rpx;
+          }
+          .mr120 {
+            margin-right: 100rpx;
+          }
+          .radio-txt {
+            font-size: 28rpx;
+            color: rgba(47, 48, 49, 0.75);
+            line-height: 42rpx;
+          }
+          .active-txt {
+            color: #2f3031;
+          }
+        }
+        .birthday {
+          width: 100%;
+          height: 72rpx;
+          display: flex;
+          align-items: center;
+          justify-content: flex-start;
+          box-sizing: border-box;
+          padding-left: 14rpx;
+          font-size: 28rpx;
+          color: #2f3031;
+        }
+        .subtitle {
+          font-size: 24rpx;
+          color: rgba(47, 48, 49, 0.5);
+          line-height: 36rpx;
+          margin-bottom: 16rpx;
+        }
+        .default-data {
+          font-size: 28rpx;
+          color: rgba(47, 48, 49, 0.5);
+          line-height: 36rpx;
+          width: 100%;
+          height: 72rpx;
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          box-sizing: border-box;
+          padding-left: 16rpx;
+        }
+        .upload-btn {
+          width: 100%;
+          height: 160rpx;
+          background: #efefef;
+          border-radius: 24rpx;
+          border: 1rpx solid #d6d8e2;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          .choost-file {
+            width: 207rpx;
+            height: 56rpx;
+            background: #ffffff;
+            border-radius: 44rpx;
+            border: 1rpx solid #e5e6e6;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            font-size: 24rpx;
+            color: rgba(47, 48, 49, 0.75);
+            line-height: 36rpx;
+          }
+        }
+        .mb4 {
+          margin-bottom: 4rpx !important;
+        }
+        .same-textarea {
+          width: 100%;
+          height: 240rpx;
+          box-sizing: border-box;
+          padding: 12rpx;
+          background: #ffffff;
+          border-radius: 12rpx;
+          border: 1rpx solid #e5e6e6;
+        }
+      }
+    }
+  }
+  .empty-box {
+    width: 100%;
+    height: 170rpx;
+  }
+  .foot-box {
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    box-sizing: border-box;
+    padding: 32rpx 32rpx 52rpx 32rpx;
+    background: #ffffff;
+    z-index: 88;
+    .submit-btn {
+      width: 100%;
+      height: 88rpx;
+      background: #2969ff;
+      border-radius: 12rpx;
+      font-size: 32rpx;
+      color: #ffffff;
+      line-height: 48rpx;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+}
 </style>
--- campusPages/parter/list/index.vue
+++ campusPages/parter/list/index.vue
@@ -1,18 +1,218 @@
 <template>
-  
+  <view class="wripper">
+    <scroll-view class="list" scroll-x="false" scroll-y="true">
+      <view class="item" v-for="(item, key) in list" :key="key">
+        <image
+          class="photo"
+          v-if="item.profilePhoto"
+          :src="item.profilePhoto"
+        />
+        <image
+          class="photo"
+          v-else
+          :src="
+            item.gender === 'male'
+              ? imgUrl + '/partner/img_photo_male.png'
+              : imgUrl + '/partner/img_photo_female.png'
+          "
+        />
+
+        <view class="info">
+          <view class="row mb4">
+            <view class="name">{{ item.reallyName }}</view>
+            <view class="grade">
+              <image :src="imgUrl + '/campus/icon_xl.png'" class="hat" />
+              <view class="level">{{ item.highestEdu }}</view>
+            </view>
+          </view>
+          <view class="row mb5">
+            <text class="major">专业</text>
+            <view class="cut"></view>
+            <text class="major">{{ item.major }}</text>
+          </view>
+          <view class="partner">校园合伙人</view>
+
+          <view class="row">
+            <image
+              :src="imgUrl + '/campus/icon_xxmc.png'"
+              class="icon-school"
+            />
+            <view class="school-name">{{ item.schoolName }}</view>
+          </view>
+        </view>
+      </view>
+    </scroll-view>
+  </view>
 </template>
 
 <script>
+import { imgUrls } from "@/config/app";
+
 export default {
   data: function () {
-    return {};
+    return {
+      imgUrl: imgUrls,
+      pageIndex: 1,
+      list: [
+        {
+          _id: "25e993b766d8089b099d8f29729e59bc",
+          birthday: "2003-08-21",
+          referrerName: "毛邦铭",
+          owner: "1803248890808373250",
+          referrerContact: "18518089514",
+          gender: "female",
+          partnerLevel: "partner",
+          mobile: "15600492003",
+          reviewers: [],
+          createdAt: 1725434011336,
+          createBy: "1803248890808373250",
+          major: "经济学（辅修大数据）",
+          wechatNum: "Grace201201",
+          updateBy: "1803248890808373250",
+          reallyName: "黄亦玟",
+          _openid: "1803248890808373250",
+          hasReferrer: "true",
+          gradYear: "2026",
+          schoolName: "中国农业大学",
+          wechatName: "黄亦玟",
+          highestEdu: "本科",
+          email: "huangyiwen6@163.com",
+          status: "pending",
+          updatedAt: 1725434011336,
+        },
+        {
+          _id: "2ad666ce66d80a1809a553ed14b3b29b",
+          birthday: "1998-10-09",
+          referrerContact: "18518089514",
+          gender: "male",
+          partnerLevel: "partner",
+          createdAt: 1725434392871,
+          profilePhoto:
+            "cloud://prod-cloud-env-9gqq29j68db5f470.7072-prod-cloud-env-9gqq29j68db5f470-1326719267/weda-uploader/0aeee364c5cf1ca6fa523af1a91f91b1-微信图片_20240904151852.jpg",
+          major: "绿色化学",
+          updateBy: "1803248890808373250",
+          reallyName: "郑云潇",
+          hasReferrer: "true",
+          schoolName: "帝国理工学院",
+          wechatName: "郑云潇101",
+          highestEdu: "硕士",
+          email: "zheng_yunxiao101@163.com",
+          updatedAt: 1725434392871,
+          referrerName: "毛邦铭",
+          owner: "1803248890808373250",
+          mobile: "15811480934",
+          reviewers: [],
+          createBy: "1803248890808373250",
+          wechatNum: "wxid_sd9axb9fb0si22",
+          _openid: "1803248890808373250",
+          gradYear: "2023",
+          status: "pass",
+        },
+      ],
+    };
   },
-  methods: {
-    
-  },
+  methods: {},
 };
 </script>
 
 <style lang="scss" scoped>
+page {
+  background: #f4f4f4;
+}
+.wripper {
+  width: 100%;
+  box-sizing: border-box;
+  padding: 24rpx;
 
+  .item {
+    width: 100%;
+    background: #ffffff;
+    box-shadow: 0rpx 4rpx 8rpx -4rpx rgba(0, 0, 0, 0.08);
+    border-radius: 24rpx;
+    box-sizing: border-box;
+    padding: 24rpx;
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 24rpx;
+    .photo {
+      width: 200rpx;
+      height: 200rpx;
+      border-radius: 20rpx;
+      margin-right: 24rpx;
+    }
+    .info {
+      display: flex;
+      flex-direction: column;
+      .row {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        .name {
+          font-size: 34rpx;
+          color: #2f3031;
+          line-height: 52rpx;
+          font-weight: bold;
+        }
+        .grade {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          background: #f2e4cd;
+          border-radius: 8rpx;
+          margin-left: 17rpx;
+          box-sizing: border-box;
+          padding: 5rpx 8rpx;
+          height: 36rpx;
+          .hat {
+            width: 24rpx;
+            height: 22rpx;
+            margin-right: 4rpx;
+          }
+          .level {
+            font-size: 24rpx;
+            color: #89633f;
+            line-height: 36rpx;
+          }
+        }
+      }
+      .major {
+        font-size: 24rpx;
+        color: rgba(47, 48, 49, 0.75);
+        line-height: 36rpx;
+      }
+      .cut {
+        width: 1rpx;
+        height: 24rpx;
+        background: #e5e6e6;
+        margin-left: 12rpx;
+        margin-right: 12rpx;
+      }
+      .mb4 {
+        margin-bottom: 4rpx;
+      }
+      .mb5 {
+        margin-bottom: 5rpx;
+      }
+
+      .partner {
+        font-size: 24rpx;
+        color: #2f3031;
+        line-height: 36rpx;
+        margin-bottom: 23rpx;
+      }
+      .icon-school {
+        width: 24rpx;
+        height: 27rpx;
+
+        margin-right: 8rpx;
+      }
+      .school-name {
+        font-size: 24rpx;
+        color: #2f3031;
+        line-height: 36rpx;
+        margin-right: 7rpx;
+      }
+    }
+  }
+}
 </style>
